55a71803ff1118b63e2f2c14fdaee606527a7546
Taras Gez <taraspixel@gmail.com>
2024-10-18 15:01:04 +0200

    add eslint, prettier, nextui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import '@nextui-org/react/dist/style.css';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
