8b1e2bf6b78fff4ee4f18c8a45f701276369e37f
petelkavictor <p.v.victor322@gmail.com>
2024-11-03 22:18:21 +0200

    fix

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -8,6 +8,9 @@ describe('OAuth', function () {
     cy.get('a').contains('Sign In').should('exist').click()
     cy.get('button').contains('Signin with Github').should('exist').click()
 
+    cy.visit(
+      'https://github.com/login?client_id=Ov23liezSMqaZLQWyxQb&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3DOv23liezSMqaZLQWyxQb%26code_challenge%3DtTPUn3MMvcmFr3F_mcK6Gl6jfw5jt5juNGwEoE4_xGo%26code_challenge_method%3DS256%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fapi%252Fauth%252Fcallback%252Fgithub%26response_type%3Dcode%26scope%3Dread%253Auser%2Buser%253Aemail'
+    )
     cy.origin('https://github.com', () => {
       cy.get('input[name="login"]').type('testovazapus123@gmail.com')
       cy.get('input[name="password"]').type('test_123Password')
