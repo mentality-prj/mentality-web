1f540eefeb6380da004fbdc6ce3349a757dba799
petelkavictor <p.v.victor322@gmail.com>
2024-11-03 21:46:13 +0200

    cypress github test

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,5 +1,19 @@
-describe('Google', function () {
+describe('OAuth', function () {
   it('OAuth Google', function () {
     cy.loginByGoogleApi()
+  })
+
+  it('OAuth GitHub', () => {
+    cy.visit('/')
+    cy.get('a').contains('Sign In').should('exist').click()
+    cy.get('button').contains('Signin with Github').should('exist').click()
+
+    cy.origin('https://github.com', () => {
+      cy.get('input[name="login"]').type('testovazapus123@gmail.com')
+      cy.get('input[name="password"]').type('test_123Password')
+      cy.get('input[name="commit"]').contains('Sign in').should('exist').click()
+    })
+
+    cy.visit('/')
   })
 })
