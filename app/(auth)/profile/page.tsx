099c91b75e5dfc9f304d918470962233fa145475
Taras Gez <taraspixel@gmail.com>
2024-11-07 18:12:22 +0100

    add validate token

--- app/(auth)/profile/page.tsx
+++ app/(auth)/profile/page.tsx
@@ -1,10 +1,29 @@
-import { auth } from '@/auth'
+import axios from 'axios'
+
+import { auth, CustomSession } from '@/auth'
 import { SignOut } from '@/components/signout-button'
 import { mockProfileData } from '@/mockApi.js'
 
 export default async function ProfilePage() {
-  const session = await auth()
+  const session = (await auth()) as CustomSession
   const data = session?.accessToken ? await mockProfileData(session?.accessToken) : null
+
+  const sendTokenToBackend = async () => {
+    'use server'
+    console.log('session', session)
+
+    if (session?.idToken) {
+      try {
+        const response = await axios.post('http://localhost:3200/api/auth/validate-token', {
+          token: session.idToken,
+          provider: session.provider,
+        })
+        console.log('User data from backend:', response.data)
+      } catch (error) {
+        console.error('Error validating token:', error)
+      }
+    }
+  }
 
   return (
     <div className="flex flex-col items-center gap-10">
@@ -12,9 +31,13 @@ export default async function ProfilePage() {
         <div className="flex flex-col gap-5">
           <div className="">{data.name}</div>
           <div className="">{data.email}</div>
-
-          {data.image && <img src={data.image} />}
         </div>
+      )}
+
+      {session && (
+        <form action={sendTokenToBackend}>
+          <button type="submit">Validate Token</button>
+        </form>
       )}
       <SignOut />
     </div>
--- auth.ts
+++ auth.ts
@@ -1,11 +1,24 @@
-import NextAuth from 'next-auth'
+import NextAuth, { Session } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 
-declare module 'next-auth' {
-  interface Session {
-    accessToken?: string
-  }
+interface CustomJWT extends JWT {
+  accessToken?: string
+  expiresIn?: number
+  idToken?: string
+  provider?: string
+  tokenType?: string
+  type?: string
+}
+
+export interface CustomSession extends Session {
+  accessToken?: string
+  expiresIn?: number
+  idToken?: string
+  provider?: string
+  tokenType?: string
+  type?: string
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -19,18 +32,25 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return !!auth
     },
     async jwt({ token, account }) {
-      // Якщо токен OAuth успішно отримано, додаємо його до токена сесії
+      // If the OAuth token is successfully received, we add it to the session token
+
       if (account) {
-        token.accessToken = account.access_token
+        token.accessToken = account.access_token as string
+        token.idToken = account.id_token as string
+        token.expiresIn = account.expires_in as number
+        token.provider = account.provider as string
+        token.tokenType = account.token_type as string
+        token.type = account.type as string
       }
-      return token
+      return token as CustomJWT
     },
-    async session({ session, token }) {
-      // Додаємо токен до сесії, щоб можна було отримати його на фронтенді
-      if (typeof token?.accessToken === 'string') {
-        session.accessToken = token.accessToken
+    async session({ session, token }: { session: CustomSession; token: CustomJWT }) {
+      // Add the token to the session so that it can be obtained on the frontend
+      const { idToken } = token as CustomJWT
+      if (idToken) {
+        session.idToken = idToken
       }
-      return session
+      return session as CustomSession
     },
   },
 })
