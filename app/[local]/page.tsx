5213c85308403c63166fa4b817a5200ff1748099
petelkavictor <p.v.victor322@gmail.com>
2024-11-10 16:00:51 +0200

    middleware

--- app/[local]/page.tsx
+++ app/[local]/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@nextui-org/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useTranslations } from 'next-intl'
 
 export default function Home() {
+  const t = useTranslations('HomePage')
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
@@ -48,6 +51,7 @@ export default function Home() {
           </a>
         </div>
         <Button color="primary">Button from NextUI</Button>
+        <h1>{t('title')}</h1>
         <Link href="/signin">Sign In</Link>
         <Link href="/profile">Profile</Link>
       </main>
