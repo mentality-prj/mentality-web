6eafd8546448739de95d8c4a9acc663620101e61
petelkavictor <p.v.victor322@gmail.com>
2024-11-11 09:46:19 +0200

    middleware fix for OAuth and Internationalization

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,19 @@
+import { NextRequest } from 'next/server'
 import createMiddleware from 'next-intl/middleware'
+
+import { auth } from '@/auth'
 
 import { routing } from './i18n/routing'
 
-export default createMiddleware(routing)
+export async function middleware(request: NextRequest) {
+  const data = await auth()
+
+  if (!data?.user && request.nextUrl.pathname.startsWith('/profile')) {
+    return Response.redirect(new URL('/signin', request.url)) // Перенаправляємо на сторінку авторизації
+  }
+
+  return createMiddleware(routing)(request)
+}
 
 export const config = {
   matcher: ['/', '/(en|id)', '/profile', '/signin'],
