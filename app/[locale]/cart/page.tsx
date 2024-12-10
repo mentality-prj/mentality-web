e205be735bd57c50b6fd6da83b190c36fecf5d76
petelkavictor <p.v.victor322@gmail.com>
2024-12-10 17:55:32 +0200

    build(other): add cookies

--- app/[locale]/cart/page.tsx
+++ app/[locale]/cart/page.tsx
@@ -1,15 +1,8 @@
-import { auth } from '@/auth'
 import CartList from '@/components/CartList'
 import { mockCartData } from '@/mockApi'
-import { CustomSession } from '@/types/auth'
 
 export default async function CartPage() {
-  const session = (await auth()) as CustomSession
-  const data = session?.OAuthToken ? await mockCartData(session?.OAuthToken) : null
+  const data = await mockCartData()
 
-  return (
-    <div className="mx-auto max-w-7xl">
-      <CartList data={data} />
-    </div>
-  )
+  return <div className="mx-auto max-w-7xl">{data ? <CartList data={data} /> : <div>Your cart is empty</div>}</div>
 }
