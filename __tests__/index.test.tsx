f2b6acf04cfc3644808123f56a947e28ad3bc232
petelkavictor <p.v.victor322@gmail.com>
2024-12-15 17:06:12 +0200

    fix(other): fixed tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,19 +1,17 @@
-// import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
-// import Home from '@/app/[locale]/page'
-describe('fix tests', () => {
-  it('test', () => {})
+import Home from '@/app/[locale]/page'
+
+describe('Home Page', () => {
+  it('displays the "Available Balance" block', () => {
+    render(<Home />)
+    const text = screen.getByText(/Available Balance/i)
+    expect(text).toBeInTheDocument()
+  })
+
+  it('displays the "Section" block', () => {
+    render(<Home />)
+    const text = screen.getByText(/Section/i)
+    expect(text).toBeInTheDocument()
+  })
 })
-// describe('Home Page', () => {
-//   it('displays the "Available Balance" block', () => {
-//     render(<Home />)
-//     const text = screen.getByText(/Available Balance/i)
-//     expect(text).toBeInTheDocument()
-//   })
-
-//   it('displays the "Section" block', () => {
-//     render(<Home />)
-//     const text = screen.getByText(/Section/i)
-//     expect(text).toBeInTheDocument()
-//   })
-// })
