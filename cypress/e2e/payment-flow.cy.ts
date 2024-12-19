da40c62eaa35f05e5921c22199ce56722e1bd085
petelkavictor <p.v.victor322@gmail.com>
2024-12-19 23:13:01 +0200

    test(other): tests fix

--- cypress/e2e/payment-flow.cy.ts
+++ cypress/e2e/payment-flow.cy.ts
@@ -8,23 +8,27 @@ describe('Payment flow', () => {
     cy.contains('button', 'Proceed to Checkout').should('exist').click()
 
     //Delivery-details form
-    cy.get('input[name="fullName"]').click().type('Test string')
-    cy.get('input[name="city"]').click().type('Київ')
+    cy.get('input[name="fullName"]').should('exist').click().type('Test string')
+    cy.get('input[name="city"]').should('exist').click().type('Київ')
     cy.contains('button', 'м. Київ, Київська обл.').should('exist').click()
     cy.get('input[name="city"]').should('have.value', 'м. Київ, Київська обл.')
-    cy.get('input[name="deliveryMethod"][value="courier-delivery"]').click()
+    cy.get('input[name="deliveryMethod"][value="courier-delivery"]').should('exist').click()
     cy.get('input[name="deliveryMethod"][value="courier-delivery"]').should('be.checked')
     cy.get('div').contains('mm').click().type('12122040')
     cy.get('button').contains('Submit').click()
 
     //Payment-info form
-    cy.get('input[name="cardNumber"]').click().type('1234567812345678')
-    cy.contains('button', 'select month').should('exist').click()
+    cy.get('input[name="cardNumber"]').should('exist', { timeout: 10000 }).click().type('1234567812345678')
+    cy.contains('button', 'select month').should('exist', { timeout: 10000 }).click()
     cy.contains('span', '01').should('exist').click()
-    cy.contains('button', 'select year').should('exist').click()
-    cy.contains('span', '2028').should('exist').click()
-    cy.get('input[name="cvv"]').click().type('123')
-    cy.get('button').contains('Submit').click()
+    cy.contains('button', 'select year').should('exist', { timeout: 10000 }).click()
+    cy.contains('span', '2028').should('exist', { timeout: 10000 }).click()
+    cy.wait(1000)
+    cy.get('body').click(0, 0)
+    cy.wait(1000)
+    cy.get('input[name="cvv"]').should('exist', { timeout: 10000 }).click().type('123')
+    cy.get('input[name="cvv"]').should('have.value', '123')
+    cy.get('button').should('exist', { timeout: 10000 }).contains('Submit').click()
 
     //Review form
     cy.contains('p', 'full name:').should('exist', { timeout: 10000 })
