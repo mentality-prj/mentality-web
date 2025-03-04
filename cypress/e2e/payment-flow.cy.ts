dc9a94c34eeb83aac2157158815243cf9a1bf810
petelkavictor <p.v.victor322@gmail.com>
2025-03-04 14:47:09 +0200

    fix: cypress test

--- cypress/e2e/payment-flow.cy.ts
+++ cypress/e2e/payment-flow.cy.ts
@@ -5,13 +5,8 @@ describe('Cart', () => {
         'cart',
         JSON.stringify([
           {
-            id: 1,
-            name: 'Fjallraven',
-            price: 109.95,
-            description:
-              'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-
-            image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+            id: '1',
+            quantity: 1,
           },
         ])
       )
