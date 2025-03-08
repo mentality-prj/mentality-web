db289f14b33a7d0f4b8f490dfbd0595d67b78602
Taras Gez <taraspixel@gmail.com>
2025-03-09 00:41:15 +0100

    feat: add unpublished tips

--- requests/tips.ts
+++ requests/tips.ts
@@ -47,3 +47,48 @@ export async function addTip(user: CustomUser, prompt: string, lang: SupportedLa
     }
   }
 }
+
+export const getUnpablishedTips = async (user: CustomUser) => {
+  let error = new Error()
+
+  if (user && user.role === Roles.ADMIN) {
+    try {
+      const response = await fetch(`${APIUrl}/tips/unpublished`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+
+      if (!response.ok) {
+        const errorData = await response.json()
+
+        // debt: add logger
+        switch (response.status) {
+          case 401:
+            // You are not authorized. Please log in.
+            error = { name: 'Unauthorized:', message: errorData.message }
+            break
+          case 500:
+            // 'An internal server error occurred. Please try again later.'
+            error = { name: 'Server Error:', message: errorData.message }
+            break
+          default:
+            // Unknown Error
+            error = { name: 'Error:', message: errorData.message }
+        }
+
+        if (error) {
+          console.log('addTip error', error)
+        }
+      } else {
+        const data = await response.json()
+        console.log('RESPONSE: ', data)
+        return data
+      }
+    } catch (err) {
+      console.log('error', err)
+    }
+  }
+}
