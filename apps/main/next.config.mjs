a7911460c6dcbe31e879f780508ac0692c00c5eb
petelkavictor <p.v.victor322@gmail.com>
2025-03-22 14:20:51 +0200

    build(other): config next.config

--- apps/main/next.config.mjs
+++ apps/main/next.config.mjs
@@ -4,6 +4,14 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/shop/:path*',
+        destination: 'http://localhost:3001/shop/:path*', // Proxy to the Shop app
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
