d581f9763f6db6d705c8406f5cf6180be9efbf31
Dima <dimaklim04@gmail.com>
2025-04-05 20:51:47 +0300

    style: add colors from Figma to global.css and refactor components to use the new color variables

--- app/[locale]/(auth)/profile/layout.tsx
+++ app/[locale]/(auth)/profile/layout.tsx
@@ -5,7 +5,7 @@ import Sidebar from '@/components/Layout/sidebar'
 
 const ProfileLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="grid h-full grid-cols-[263px_auto] grid-rows-[60px_auto] gap-6 bg-container-primary">
+    <div className="grid h-full grid-cols-[263px_auto] grid-rows-[60px_auto] gap-6 bg-gray-95">
       <Sidebar />
       <Header />
       <main className="col-start-2 w-full">{children}</main>
--- ds/shadcn/button.tsx
+++ ds/shadcn/button.tsx
@@ -5,15 +5,15 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md leading-none font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-focus  disabled:pointer-events-none disabled:text-muted-foreground [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md leading-none font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-focus disabled:pointer-events-none disabled:text-gray-30 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground hover:bg-primary-hover active:bg-primary-pressed disabled:bg-secondary-muted focus-visible:ring-offset-4 focus-visible:bg-primary-focus disabled:bg-secondary-muted',
+        primary:
+          'bg-primary text-primary-foreground hover:bg-primary-hover active:bg-primary-pressed disabled:bg-secondary-muted focus-visible:ring-offset-4 focus-visible:bg-primary-focus disabled:bg-gray-90',
         // destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/80',
         secondary:
-          'focus-visible:ring-1 focus-visible:ring-primary-focus focus-visible:ring-offset-4 focus-visible:border-primary-focus focus-visible:bg-secondary-state focus-visible:text-ring border border-primary bg-transparent text-primary hover:bg-secondary-hover hover:border-primary-hover hover:text-primary-hover active:bg-secondary-pressed active:text-primary-foreground active:border-secondary-pressed disabled:border-muted-foreground disabled:bg-secondary-muted ',
+          'focus-visible:ring-offset-4 focus-visible:border-primary-focus focus-visible:bg-secondary focus-visible:text-primary-focus border border-primary bg-transparent text-primary hover:bg-secondary-hover hover:border-primary-hover hover:text-primary-hover active:bg-secondary-focus active:text-primary-pressed active:border-secondary-pressed disabled:border-gray-30 disabled:bg-gray-90',
         outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         // ghost: 'hover:bg-accent hover:text-accent-foreground',
         // link: 'text-primary underline-offset-4 hover:underline',
@@ -26,7 +26,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default',
     },
   }
