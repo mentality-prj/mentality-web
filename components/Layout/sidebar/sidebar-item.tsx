4ef674f46400a97ff088de64b0c82db5e380c508
Dima <dimaklim04@gmail.com>
2025-04-06 16:59:39 +0300

    refactor: add sidebar button

--- components/Layout/sidebar/sidebar-item.tsx
+++ components/Layout/sidebar/sidebar-item.tsx
@@ -1,3 +1,4 @@
+import { Slot } from '@radix-ui/react-slot'
 import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
@@ -11,15 +12,31 @@ interface SidebarItemProps {
 
 export const SidebarItem = ({ href, icon, label, isActive = false }: SidebarItemProps) => (
   <li>
-    <Link
-      href={href}
+    <SidebarItemButton
+      asChild
       className={cn(
-        'flex items-center gap-2 rounded-lg bg-transparent py-2 pl-4 font-medium transition-colors hover:bg-secondary hover:text-primary-hover focus-visible:bg-secondary focus-visible:text-primary-focus focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-focus active:bg-secondary-pressed active:text-white [&_svg_circle]:transition-colors [&_svg_circle]:hover:stroke-primary-hover [&_svg_circle]:focus-visible:stroke-primary-focus [&_svg_circle]:active:stroke-white [&_svg_path]:transition-colors [&_svg_path]:hover:stroke-primary-hover [&_svg_path]:focus-visible:stroke-primary-focus [&_svg_path]:active:stroke-white',
         isActive ? 'bg-secondary text-primary [&_svg_circle]:stroke-primary [&_svg_path]:stroke-primary' : ''
       )}
     >
-      {icon}
-      <span>{label}</span>
-    </Link>
+      <Link href={href}>
+        {icon}
+        <span>{label}</span>
+      </Link>
+    </SidebarItemButton>
   </li>
 )
+interface SidebarItemButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean
+}
+export const SidebarItemButton = ({ asChild, className, ...props }: SidebarItemButtonProps) => {
+  const Comp = asChild ? Slot : 'button'
+  return (
+    <Comp
+      className={cn(
+        'flex items-center gap-2 rounded-lg bg-transparent py-2 pl-4 font-medium transition-colors hover:bg-secondary hover:text-primary-hover focus-visible:bg-secondary focus-visible:text-primary-focus focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-focus active:bg-secondary-pressed active:text-white [&_svg_circle]:transition-colors [&_svg_circle]:hover:stroke-primary-hover [&_svg_circle]:focus-visible:stroke-primary-focus [&_svg_circle]:active:stroke-white [&_svg_path]:transition-colors [&_svg_path]:hover:stroke-primary-hover [&_svg_path]:focus-visible:stroke-primary-focus [&_svg_path]:active:stroke-white',
+        className
+      )}
+      {...props}
+    />
+  )
+}
