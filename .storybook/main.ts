c7d27782734e6857c5eb01eff6bf118d26ed68a7
Oksana Gez <39303694+OksanaSanag@users.noreply.github.com>
2025-04-09 11:21:57 +0200

    Add storybook figma tokin (#32)
    * add figma in storybook
    
    * feat: integrate Figma API into Storybook using Personal Access Token
    
    * add figma in storybook
    
    * feat: integrate Figma API into Storybook using Personal Access Token
    
    * add figma in storybook
    
    * feat: integrate Figma API into Storybook using Personal Access Token
    
    * chore: deleted old storybook test files
    
    * chore: update URL in Button.stories.ts
    
    ---------
    
    Co-authored-by: petelkavictor <p.v.victor322@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,20 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-designs',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+  docs: {
+    defaultName: 'Documentation',
   },
 }
 export default config
