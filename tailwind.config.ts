672a9a5fc3f41b23f5b07f937148b2cfdd82d2af
petelkavictor <p.v.victor322@gmail.com>
2025-04-17 12:38:09 +0300

    fix(other): fix fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,10 @@ const config: Config = {
           success: 'hsl(var(--text-success))',
         },
       },
+      fontFamily: {
+        sans: ['var(--font-sans)', 'sans-serif'],
+        mono: ['var(--font-mono)', 'monospace'],
+      },
       borderRadius: {
         lg: 'var(--radius)',
         xlg: 'calc(var(--radius) * 3)',
