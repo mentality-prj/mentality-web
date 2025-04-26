f55288f9a87ed6ffdd6e17ca953829c65289d5eb
petelkavictor <p.v.victor322@gmail.com>
2025-04-26 22:54:57 +0300

    fix(other): fix authentication (#35)

--- components/Buttons/index.ts
+++ components/Buttons/index.ts
@@ -1,6 +1,5 @@
 import GoToLoginPageButton from './GoToLoginPageButton'
-import SignInButton from './SignInButton'
 import SignOut from './SignOut'
 import SignOutButton from './SignOutButton'
 
-export { GoToLoginPageButton, SignInButton, SignOut, SignOutButton }
+export { GoToLoginPageButton, SignOut, SignOutButton }
