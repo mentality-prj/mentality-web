a30ca3e3d1594a9bb58ec41ef5d6ef3548ba56c8
petelkavictor <p.v.victor322@gmail.com>
2025-05-01 23:17:18 +0300

    style: update radio-group (#43)

--- ds/shadcn/radio-group.tsx
+++ ds/shadcn/radio-group.tsx
@@ -22,13 +22,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        'focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+        'group aspect-square h-5 w-5 rounded-full border-[1.5px] border-iconcolor-primary bg-transparent text-transparent outline-none hover:border-primary hover:bg-secondary-hover focus-visible:border-primary focus-visible:bg-secondary-focus focus-visible:ring-4 focus-visible:ring-secondary-focus active:bg-secondary-pressed disabled:cursor-not-allowed disabled:border-disable data-[state=checked]:border-primary data-[state=checked]:hover:!border-primary-hover data-[state=checked]:hover:bg-transparent data-[state=checked]:focus-visible:border-primary-focus data-[state=checked]:active:!border-primary-pressed',
         className
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-3.5 w-3.5 fill-primary" />
+        <Circle className="h-3 w-3 fill-primary group-hover:fill-primary-hover group-focus-visible:fill-primary-focus group-active:fill-primary-pressed group-disabled:fill-disable" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
