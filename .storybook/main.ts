06d856d99d915ac3fc6e46080cd2df5ae07ba995
petelkavictor <p.v.victor322@gmail.com>
2025-05-01 16:22:03 +0300

    feat: setup addon pseudo states

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-designs',
+    'storybook-addon-pseudo-states',
   ],
   framework: {
     name: '@storybook/nextjs',
