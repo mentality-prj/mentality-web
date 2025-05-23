4ae0b444d7b6b577d98c11f13280e730773ded1b
petelkavictor <p.v.victor322@gmail.com>
2025-05-23 14:11:54 +0300

    style: blog-card

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,8 +1,9 @@
+import { BlogCard } from '@/components/Dashboard/BlogCard'
 import { DailyCard } from '@/components/Dashboard/DailyCard'
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-6">
       <div className="flex gap-6">
         <DailyCard
           textContent="Every day, I grow stronger, wiser, and more confident in my journey."
@@ -11,6 +12,12 @@ export default function Dashboard() {
         <DailyCard
           textContent="Start your day by setting one small, achievable goal—it builds momentum for bigger successes!"
           type="tip"
+        />
+      </div>
+      <div className="flex gap-4">
+        <BlogCard
+          title="Forem ipsum dolor sit amet, consectetur adipiscing elit."
+          textContent="Dorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulpel lkdoaj"
         />
       </div>
     </div>
---
+++ components/Dashboard/BlogCard.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@/ds/shadcn/button'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/ds/shadcn/card'
+
+type BlogCardProps = {
+  title: string
+  textContent: string
+}
+
+export const BlogCard = ({ title, textContent }: BlogCardProps) => {
+  return (
+    <Card className="flex max-w-[312px] flex-col bg-white shadow-none">
+      <CardHeader className="rounded-sm p-0">
+        <img
+          className="rounded-t-sm"
+          alt="title-image"
+          src="https://png.pngtree.com/png-vector/20220725/ourmid/pngtree-close-up-huge-hands-hold-woman-victim-give-help-or-first-png-image_6073336.png"
+        />
+      </CardHeader>
+      <CardContent className="mb-6 flex flex-grow flex-col gap-3 p-6 pb-0">
+        <CardTitle className="break-words text-left text-xl/6 font-semibold">{title}</CardTitle>
+        <p className="line-clamp-2 text-sm font-normal">{textContent}</p>
+      </CardContent>
+      <CardFooter className="p-0 px-6 pb-6">
+        <Button className="max-h-4 p-0" variant="textButton">
+          Learn more
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
