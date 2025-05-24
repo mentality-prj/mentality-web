03d7c8f9863660f509be0e6ec74464504015a8e4
Oksana Gez <39303694+OksanaSanag@users.noreply.github.com>
2025-05-24 17:09:48 +0200

    fix: correct label&helperText in textarea (#55)

--- ds/shadcn/textarea-with-label.tsx
+++ ds/shadcn/textarea-with-label.tsx
@@ -1,16 +1,19 @@
-import * as React from 'react'
+import { ComponentProps } from 'react'
 
 import { Label } from './label'
 import { Textarea } from './textarea'
 
-export function TextareaWithLabel(props: React.ComponentProps<typeof Textarea>) {
+type Props = ComponentProps<typeof Textarea> & {
+  label?: string
+  helper?: string
+}
+
+export function TextareaWithLabel({ label, helper, id = 'textarea', ...props }: Props) {
   return (
-    <div>
-      <div className="flex flex-col">
-        <Label htmlFor="forTextarea">Label for Textarea</Label>
-        <Textarea id="forTextarea" {...props} />
-        <p className="mt-1 h-[14px] text-[12px] font-normal text-textcolor-tertiary">Helper text</p>
-      </div>
+    <div className="flex flex-col">
+      {label && <Label htmlFor={id}> {label} </Label>}
+      <Textarea id={id} {...props}></Textarea>
+      {helper && <p className="mt-1 h-[14px] text-[12px] font-normal text-textcolor-tertiary">{helper}</p>}
     </div>
   )
 }
