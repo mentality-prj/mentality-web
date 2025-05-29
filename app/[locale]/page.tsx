87b24498c471cae65a5634008eaeee285259d663
petelkavictor <p.v.victor322@gmail.com>
2025-05-30 02:37:33 +0300

    feat: place DailyCard components on the home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { DailyCard } from '@/components/Home/DailyCard'
+import { mockDashboardDailyData } from '@/REST/mockApi'
+
+export default async function Home() {
+  const dailyData = await mockDashboardDailyData()
   return (
     <div className="flex flex-col gap-12">
       <div className="">{/* Greeting Title component */}</div>
@@ -10,8 +14,9 @@ export default function Home() {
         </div>
         <div className="flex-col gap-8">
           <div className="flex gap-6">
-            {/* Your daily affirmation component */}
-            {/* Tip of the day */}
+            {dailyData.map(({ ...props }, id) => (
+              <DailyCard key={id} {...props} />
+            ))}
           </div>
           <div className="">{/* Chat with an AI assistant compoent */}</div>
           <div className="">{/* My progress */}</div>
