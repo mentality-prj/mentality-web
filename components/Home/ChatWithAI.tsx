c6b8b4186288117dc218beb53e4b00d89fa33250
petelkavictor <p.v.victor322@gmail.com>
2025-06-03 16:35:07 +0300

    chore: add internalization

--- components/Home/ChatWithAI.tsx
+++ components/Home/ChatWithAI.tsx
@@ -1,3 +1,5 @@
+import { useTranslations } from 'next-intl'
+
 import { CustomCard } from '@/ds/components/CustomCard'
 import { CloudEmoji } from '@/ds/icons/emoji/cloud'
 import { HeartEmoji } from '@/ds/icons/emoji/heart'
@@ -6,17 +8,28 @@ import { HumanEmoji } from '@/ds/icons/emoji/human'
 import { SectionCard } from '../ui/SectionCard'
 
 export const ChatWithAI = () => {
+  const t = useTranslations('HomePage.ChatWithAI')
   return (
-    <SectionCard title="Чат з AI-помічником" subtitle="Отримай підтримку та поради">
+    <SectionCard title={t('title')} subtitle={t('subtitle')}>
       <div className="mt-2 flex flex-col gap-6">
-        <div className="rounded-md bg-surface-action p-3">&quot;Привіт! Чим я можу допомогти сьогодні?&quot;</div>
+        <div className="rounded-md bg-surface-action p-3">&quot;{t('description')}&quot;</div>
         <div className="flex flex-col gap-2">
-          <CustomCard variant="small" className="max-w-full" title="Поговорити про мої думки" icon={<CloudEmoji />} />
-          <CustomCard variant="small" className="max-w-full" title="Порадь мені вправу" icon={<HumanEmoji />} />
           <CustomCard
             variant="small"
             className="max-w-full"
-            title="Покажи турботливе повідомлення дня"
+            title={t('Talk about my thoughts')}
+            icon={<CloudEmoji />}
+          />
+          <CustomCard
+            variant="small"
+            className="max-w-full"
+            title={t('Advise me on an exercise')}
+            icon={<HumanEmoji />}
+          />
+          <CustomCard
+            variant="small"
+            className="max-w-full"
+            title={t('Show the caring message of the day')}
             icon={<HeartEmoji />}
           />
         </div>
