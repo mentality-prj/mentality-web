08c246100bcb2e4d59a4e806aee56dbcbde3f930
petelkavictor <p.v.victor322@gmail.com>
2025-06-04 00:25:39 +0300

    style: fix markup

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,31 +10,24 @@ export default async function Home() {
   const exercisesData = await mockExercisesRecoveryData()
 
   return (
-    <div className="flex flex-col gap-12">
-      <div className="">{/* Greeting Title component */}</div>
-      {/* Content ↓*/}
-      <div className="flex gap-8">
-        <div className="flex flex-col gap-8">
-          <div className="">
-            <CurrentState />
-          </div>
-          <div className="">
-            <ExercisesForRecovery exercises={exercisesData} />
-          </div>
+    <div className="flex flex-col gap-8">
+      {/* Greeting Title */}
+      <div>{/* GreetingTitle component */}</div>
+
+      {/* Main Content */}
+      <div className="mx-auto grid grid-cols-[527px_527px] gap-8">
+        <div className="flex max-w-[527px] flex-col gap-8">
+          <CurrentState />
+          <ExercisesForRecovery exercises={exercisesData} />
         </div>
-        <div className="flex flex-col gap-8">
+        <div className="flex max-w-[527px] flex-col gap-8">
           <div className="flex gap-6">
             {dailyData.map(({ ...props }, id) => (
               <DailyCard key={id} {...props} />
             ))}
           </div>
-          <div className="">
-            <ChatWithAI />
-          </div>
-
-          <div className="">
-            <MyProgress />
-          </div>
+          <ChatWithAI />
+          <MyProgress />
         </div>
       </div>
     </div>
