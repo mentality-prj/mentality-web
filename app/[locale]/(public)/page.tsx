64ee1c75ed5f3d59e0c6c3333dee1710068f9c8a
petelkavictor <p.v.victor322@gmail.com>
2025-06-14 00:53:07 +0300

    chore: add internalization

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -1,9 +1,12 @@
+import { useTranslations } from 'next-intl'
+
 import LoginButton from '@/components/Buttons/LoginButton'
 
 export default function Home() {
+  const t = useTranslations('MainPage')
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-3">
-      <p className="text-5xl">In Building</p>
+      <p className="text-5xl">{t('Coming soon')}</p>
       <LoginButton />
     </div>
   )
