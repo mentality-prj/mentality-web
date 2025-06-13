a0866be73a3cf182df43c010a61da418fd2e777e
petelkavictor <p.v.victor322@gmail.com>
2025-06-14 00:44:53 +0300

    style: main page

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -1,3 +1,10 @@
+import LoginButton from '@/components/Buttons/LoginButton'
+
 export default function Home() {
-  return <div>In Building</div>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-3">
+      <p className="text-5xl">In Building</p>
+      <LoginButton />
+    </div>
+  )
 }
