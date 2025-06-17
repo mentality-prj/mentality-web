8266f2c221b017bc92b2a76b6e979065a949238d
petelkavictor <p.v.victor322@gmail.com>
2025-06-17 16:56:01 +0300

    fix: sidebar-provider

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -3,6 +3,8 @@ import { ReactNode } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
 
+import SidebarWrapper from '@/components/Sidebar'
+
 export interface ProvidersProps {
   children: ReactNode
   themeProps?: ThemeProviderProps
@@ -12,7 +14,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   return (
     <SessionProvider>
       <NextThemesProvider defaultTheme="system" attribute="class" {...themeProps}>
-        {children}
+        <SidebarWrapper>{children}</SidebarWrapper>
       </NextThemesProvider>
     </SessionProvider>
   )
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,18 +3,14 @@ import { ReactNode } from 'react'
 
 import Content from '@/components/wrappers/Content'
 
-import SidebarWrapper from '../Sidebar'
-
 interface MainLayoutProps {
   children: ReactNode
 }
 
 const Layout = ({ children }: MainLayoutProps) => {
   return (
-    <section className="flex">
-      <SidebarWrapper>
-        <Content>{children}</Content>
-      </SidebarWrapper>
+    <section className="flex w-full">
+      <Content>{children}</Content>
     </section>
   )
 }
