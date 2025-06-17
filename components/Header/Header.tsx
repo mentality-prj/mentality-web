41e8b7dfade686b99973f06d465d685d2a912102
OksanaSanag <oksanasanag@gmail.com>
2025-06-17 16:52:47 +0200

    feat: add Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import LangSwitch from '@/components/Buttons/LangSwitch'
+
+import { LocalDate } from './LocalDate'
+import { SearchBar } from './SearchBar'
+import { ThemeToggleButton } from './ThemeToggleButton'
+
+export function Header() {
+  return (
+    <header className="flex w-full items-center justify-between rounded-b-default bg-surface-white px-8 py-4">
+      <LocalDate />
+      <div className="flex items-center gap-4">
+        <SearchBar />
+        <ThemeToggleButton />
+        <LangSwitch />
+      </div>
+    </header>
+  )
+}
---
+++ components/Header/ThemeToggleButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'ds/shadcn/button'
+import { Moon } from 'lucide-react'
+
+export const ThemeToggleButton = () => {
+  return (
+    <Button variant={'iconButton'}>
+      <Moon />
+    </Button>
+  )
+}
