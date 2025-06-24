75bdf893f91be47caf37ecd6325f3549c6ff727a
OksanaSanag <oksanasanag@gmail.com>
2025-06-24 19:18:20 +0200

    chore: add icon in textarea

--- ds/shadcn/textarea.tsx
+++ ds/shadcn/textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
       <textarea
         className={cn(
           //'placeholder:text-muted-foreground md:text-sm',
-          'disabled:border-disabled disabled:bg-disabled flex min-h-[144px] min-w-[384px] resize-none rounded-md border border-outline-secondary bg-transparent px-4 py-3 text-[14px]/[20px] font-normal text-textcolor-tertiary invalid:border-outline-error invalid:text-textcolor-primary hover:border-primary-hover focus:border-primary-focus focus:text-primary-focus focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-focus disabled:cursor-not-allowed disabled:bg-[#8E8EA4]',
+          'disabled:border-disabled disabled:bg-disabled flex resize-none rounded-md border border-outline-secondary bg-transparent px-4 py-3 text-[14px]/[20px] font-normal text-textcolor-tertiary invalid:border-outline-error invalid:text-textcolor-primary hover:border-primary-hover focus:border-primary-focus focus:text-primary-focus focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-focus disabled:cursor-not-allowed disabled:bg-[#8E8EA4]',
           className
         )}
         ref={ref}
