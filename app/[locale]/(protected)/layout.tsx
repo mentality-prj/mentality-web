6ac1d5e794b6ca3ccf4d6e89c36767943dc7a625
petelkavictor <p.v.victor322@gmail.com>
2025-06-24 12:30:01 +0300

    refactor layouts and sidebar provider (#87)
    * fix: sidebar-provider
    
    * refactor: layouts
    
    * refactor: layouts and sidebar

--- app/[locale]/(protected)/layout.tsx
+++ app/[locale]/(protected)/layout.tsx
@@ -1,36 +1,7 @@
 import { ReactNode } from 'react'
-import { Toaster } from 'react-hot-toast'
-import type { Metadata } from 'next'
 
-import AppIntlProvider from '@/app/(providers)/AppIntlProvider'
 import Layout from '@/components/Layout'
-import { fontSans } from '@/config/fonts'
 
-import { Providers } from '../providers'
-
-import '@/styles/globals.css'
-
-export const metadata: Metadata = {
-  title: 'Mentality',
-}
-
-export default function RootLayout({
-  children,
-  params: { locale },
-}: {
-  children: ReactNode
-  params: { locale: string }
-}) {
-  return (
-    <html lang={locale} suppressHydrationWarning={true} className={`${fontSans.variable} antialiased`}>
-      <body>
-        <AppIntlProvider>
-          <Providers>
-            <Toaster position="top-right" reverseOrder={false} />
-            <Layout>{children}</Layout>
-          </Providers>
-        </AppIntlProvider>
-      </body>
-    </html>
-  )
+export default function ProtectedLayout({ children }: { children: ReactNode }) {
+  return <Layout>{children}</Layout>
 }
