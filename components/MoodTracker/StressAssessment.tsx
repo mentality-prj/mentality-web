b4345381833bb3442d00e34e77e34b64576fbc0d
Oksana Gez <39303694+OksanaSanag@users.noreply.github.com>
2025-07-03 19:58:01 +0200

    feat: create StressAssessment block (#99)

--- components/MoodTracker/StressAssessment.tsx
+++ components/MoodTracker/StressAssessment.tsx
@@ -1,3 +1,15 @@
+import { useTranslations } from 'next-intl'
+
+import { Button } from '@/ds/shadcn/button'
+
+import { SectionCard } from '../ui/SectionCard'
+
 export const StressAssessment = () => {
-  return <div>StressAssessment</div>
+  const t = useTranslations('MoodTracker.StressAssessment')
+
+  return (
+    <SectionCard title={t('title')} subtitle={t('description')}>
+      <Button>{t('button')}</Button>
+    </SectionCard>
+  )
 }
