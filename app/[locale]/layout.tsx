e1d0bec78e90979ec686ef40109952a312e2c9ae
petelkavictor <p.v.victor322@gmail.com>
2025-07-14 17:20:07 +0300

    feat: add not-found page

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 import type { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
 import AppIntlProvider from '@/app/(providers)/AppIntlProvider'
 import { fontSans } from '@/config/fonts'
+import { routing } from '@/i18n/routing'
+import { SupportedLanguage } from '@/types/languages'
 
 import { Providers } from './providers'
 
@@ -13,13 +16,17 @@ export const metadata: Metadata = {
   title: 'Mentality',
 }
 
-export default function LocaleLayout({
+export default async function LocaleLayout({
   children,
   params: { locale },
 }: {
   children: ReactNode
-  params: { locale: string }
+  params: { locale: SupportedLanguage }
 }) {
+  if (!routing.locales.includes(locale)) {
+    notFound()
+  }
+
   return (
     <html lang={locale} suppressHydrationWarning={true} className={`${fontSans.variable} antialiased`}>
       <body>
