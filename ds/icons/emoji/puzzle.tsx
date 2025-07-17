9ecba5fce12f9d05de28ad7f1bf943f6be906668
petelkavictor <p.v.victor322@gmail.com>
2025-07-17 16:54:02 +0300

    style: add emoji

---
+++ ds/icons/emoji/puzzle.tsx
@@ -0,0 +1,5 @@
+export const PuzzleEmoji = () => (
+  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 23.4H22V3.4H2V23.4Z" fill="black" />
+  </svg>
+)
--- utils/getEmojiFromBackend.tsx
+++ utils/getEmojiFromBackend.tsx
@@ -1,11 +1,15 @@
 import { JSX } from 'react'
 
 import { CloudEmoji } from '@/ds/icons/emoji/cloud'
+import { HandThatWritesEmoji } from '@/ds/icons/emoji/hand-that-writes'
 import { HeadphonesEmoji } from '@/ds/icons/emoji/headphones'
 import { HeartEmoji } from '@/ds/icons/emoji/heart'
 import { HumanEmoji } from '@/ds/icons/emoji/human'
 import { LungsEmoji } from '@/ds/icons/emoji/lungs'
+import { PuzzleEmoji } from '@/ds/icons/emoji/puzzle'
 import { SOSEmoji } from '@/ds/icons/emoji/sos'
+import { StarEmoji } from '@/ds/icons/emoji/star'
+import { StarMotionEmoji } from '@/ds/icons/emoji/star-motion'
 import { EmojiKey } from '@/types/emoji'
 
 export function getEmojiFromBackend(key: EmojiKey): JSX.Element {
@@ -22,6 +26,14 @@ export function getEmojiFromBackend(key: EmojiKey): JSX.Element {
       return <CloudEmoji />
     case 'heart':
       return <HeartEmoji />
+    case 'hand-that-writes':
+      return <HandThatWritesEmoji />
+    case 'puzzle':
+      return <PuzzleEmoji />
+    case 'star':
+      return <StarEmoji />
+    case 'star-motion':
+      return <StarMotionEmoji />
     default:
       return <HeartEmoji />
   }
