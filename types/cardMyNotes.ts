d114d2aa79c6af1534590483782619fe7fe0e287
petelkavictor <p.v.victor322@gmail.com>
2025-07-17 21:46:45 +0300

    feat: made my-notes page

--- types/cardMyNotes.ts
+++ types/cardMyNotes.ts
@@ -1,7 +1,8 @@
 import { EmojiKey } from './emoji'
 
-export type cardMyNotes = {
+export type CardMyNotes = {
   title: string
   description: string
   icon: EmojiKey
+  link: string
 }
---
+++ types/cardRecommendation.ts
@@ -0,0 +1,5 @@
+export type CardRecommendation = {
+  tag: string
+  title: string
+  description: string
+}
