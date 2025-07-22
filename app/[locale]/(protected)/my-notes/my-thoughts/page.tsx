b7cd7dab8a64f583a0fbdc3f02fa7cd9bb79911b
petelkavictor <p.v.victor322@gmail.com>
2025-07-22 19:01:52 +0300

    style: add breadcrumbs component

--- app/[locale]/(protected)/my-notes/my-thoughts/page.tsx
+++ app/[locale]/(protected)/my-notes/my-thoughts/page.tsx
@@ -1,3 +1,9 @@
+import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
+
 export default function MyThoughtsPage() {
-  return <div className="">My thoughts Page</div>
+  return (
+    <div className="">
+      <Breadcrumbs currentPage="Мої думки" breadcrumbList={[{ title: 'Мої записи', href: '/my-notes' }]} />
+    </div>
+  )
 }
