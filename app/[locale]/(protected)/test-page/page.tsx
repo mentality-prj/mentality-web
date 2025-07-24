83efb26678ce3299edff003121d541ab427ec2bb
OksanaSanag <oksanasanag@gmail.com>
2025-07-24 20:11:25 +0200

    fix: func in psycho tests

--- app/[locale]/(protected)/test-page/page.tsx
+++ app/[locale]/(protected)/test-page/page.tsx
@@ -1,12 +1,12 @@
-import k10Test from '@/components/TestsQuestionnarie/consts/tests-questionnaire/k10'
-// import stressTest from '@/components/TestsQuestionnarie/consts/tests-questionnaire/stress-level'
+// import k10Test from '@/components/TestsQuestionnarie/consts/tests-questionnaire/k10'
+import stressTest from '@/components/TestsQuestionnarie/consts/tests-questionnaire/stress-level'
 import TestPageGenerator from '@/components/TestsQuestionnarie/TestPageGenerator'
 
 export default async function testPage() {
   return (
-    <div>
+    <div className="bg-reversed">
       {/* передається конкретний тест */}
-      <TestPageGenerator test={k10Test} />
+      <TestPageGenerator test={stressTest} />
     </div>
   )
 }
