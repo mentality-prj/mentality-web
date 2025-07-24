d62190e742ad68e8f177d9e2d2e131dbf0770412
OksanaSanag <oksanasanag@gmail.com>
2025-07-24 19:53:35 +0200

    style: add styles in checkbox

--- ds/shadcn/checkbox.tsx
+++ ds/shadcn/checkbox.tsx
@@ -13,12 +13,12 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'focus-visible:ring-ring data-[state=checked]:text-primary-foreground peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary',
+      'h-5 w-5 rounded-xs border border-iconcolor-primary bg-transparent outline-none hover:border-primary hover:bg-secondary-hover hover:ring-2 hover:ring-secondary-hover focus-visible:border-primary focus-visible:bg-secondary-focus focus-visible:ring-1 focus-visible:ring-primary-focus focus-visible:ring-offset-2 focus-visible:ring-offset-secondary-focus active:border-primary active:bg-secondary-pressed active:ring-2 active:ring-secondary-pressed disabled:cursor-not-allowed disabled:border-disable data-[state=checked]:border-primary data-[state=checked]:text-primary',
       className
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
+    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-primary')}>
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
