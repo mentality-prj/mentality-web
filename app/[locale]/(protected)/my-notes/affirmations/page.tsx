339e843522a7c07dc2489a9a8ab55227500c4afc
petelkavictor <p.v.victor322@gmail.com>
2025-07-29 20:03:11 +0300

    style: fill page

--- app/[locale]/(protected)/my-notes/affirmations/page.tsx
+++ app/[locale]/(protected)/my-notes/affirmations/page.tsx
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 import { useTranslations } from 'next-intl'
 
+=======
+>>>>>>> 232bb6c (style: fill page)
 import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
 import { DailyCard } from '@/components/ui/DailyCard'
 import { PageTitle } from '@/components/ui/PageTitle'
@@ -8,6 +11,7 @@ import { AstronomyIcon } from '@/ds/icons/blueIcons/astronomy'
 import { StarRingIcon } from '@/ds/icons/blueIcons/star-ring'
 
 export default function AffirmationsPage() {
+
   const t = useTranslations('AffirmationsPage')
   return (
     <div className="flex flex-col gap-8">
@@ -26,6 +30,7 @@ export default function AffirmationsPage() {
         <DailyCard variant="secondary" title={t('tip.title')} textContent={t('tip.text')} icon={<AstronomyIcon />} />
       </div>
       <SectionCard title={t('sectionCard.title')} subtitle={t('sectionCard.subtitle')} />
+
     </div>
   )
 }
