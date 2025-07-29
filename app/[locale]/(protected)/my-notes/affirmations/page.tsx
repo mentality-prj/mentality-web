ffa9b5f7f5536a1bb016f290dc8f026b372a9e11
petelkavictor <p.v.victor322@gmail.com>
2025-07-29 20:17:12 +0300

    chore: add intl

--- app/[locale]/(protected)/my-notes/affirmations/page.tsx
+++ app/[locale]/(protected)/my-notes/affirmations/page.tsx
@@ -1,3 +1,5 @@
+import { useTranslations } from 'next-intl'
+
 import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
 import { DailyCard } from '@/components/ui/DailyCard'
 import { PageTitle } from '@/components/ui/PageTitle'
@@ -6,25 +8,24 @@ import { AstronomyIcon } from '@/ds/icons/blueIcons/astronomy'
 import { StarRingIcon } from '@/ds/icons/blueIcons/star-ring'
 
 export default function AffirmationsPage() {
+  const t = useTranslations('AffirmationsPage')
   return (
     <div className="flex flex-col gap-8">
-      <Breadcrumbs currentPage="Афірмації та поради" breadcrumbList={[{ title: 'Мої записи', href: '/my-notes' }]} />
-      <PageTitle title="Афірмації та поради" subtitle="Надихайся позитивними думками кожного дня" />
+      <Breadcrumbs
+        currentPage={t('title')}
+        breadcrumbList={[{ title: `${t('ThoughtsList.title')}`, href: '/my-notes' }]}
+      />
+      <PageTitle title={t('title')} subtitle={t('subtitle')} />
       <div className="grid grid-cols-1 gap-6 laptop:grid-cols-2">
         <DailyCard
           variant="secondary"
-          title="Ваша щоденна афірмація"
-          textContent='"Кожного дня я стаю все сильнишим та впевненим на своєму шляху."'
+          title={t('affirmation.title')}
+          textContent={t('affirmation.text')}
           icon={<StarRingIcon />}
         />
-        <DailyCard
-          variant="secondary"
-          title="Порада дня"
-          textContent='"Почніть свій день із постановки однієї маленької досяжної цілі — це створює імпульс для більших успіхів!"'
-          icon={<AstronomyIcon />}
-        />
+        <DailyCard variant="secondary" title={t('tip.title')} textContent={t('tip.text')} icon={<AstronomyIcon />} />
       </div>
-      <SectionCard title="Попередні афірмації та поради" subtitle="У тебе ще не має попередніх афірмацій та порад." />
+      <SectionCard title={t('sectionCard.title')} subtitle={t('sectionCard.subtitle')} />
     </div>
   )
 }
