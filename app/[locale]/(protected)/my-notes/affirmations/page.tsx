d5dc162acb529c5bf31a8846703a7b3e419e4a68
petelkavictor <p.v.victor322@gmail.com>
2025-08-05 19:49:12 +0300

    chore: add intl

--- app/[locale]/(protected)/my-notes/affirmations/page.tsx
+++ app/[locale]/(protected)/my-notes/affirmations/page.tsx
@@ -19,13 +19,13 @@ export default function AffirmationsPage() {
       <div className="grid grid-cols-1 gap-6 laptop:grid-cols-2">
         <DailyCard
           variant="secondary"
-          toastText="Афірмація додана у Збережене"
+          toastText={t('toast.affirmation')}
           title={t('affirmation.title')}
           textContent={t('affirmation.text')}
           icon={<StarRingIcon />}
         />
         <DailyCard
-          toastText="Порада додана у Збережене"
+          toastText={t('toast.tip')}
           variant="secondary"
           title={t('tip.title')}
           textContent={t('tip.text')}
