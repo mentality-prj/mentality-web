11b9deb71d031b8356651f2a61943cb693db9698
petelkavictor <p.v.victor322@gmail.com>
2025-08-11 13:08:42 +0300

    chore: add intl

--- app/[locale]/(protected)/my-notes/saved/page.tsx
+++ app/[locale]/(protected)/my-notes/saved/page.tsx
@@ -5,14 +5,14 @@ import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
 import { PageTitle } from '@/components/ui/PageTitle'
 
 export default function SavedPage() {
-  const t = useTranslations('AffirmationsPage')
+  const t = useTranslations()
   return (
     <div className="flex flex-col gap-8">
       <Breadcrumbs
-        currentPage={'Збережене'}
-        breadcrumbList={[{ title: `${t('ThoughtsList.title')}`, href: '/my-notes' }]}
+        currentPage={t('AffirmationsPage.title')}
+        breadcrumbList={[{ title: `${t('AffirmationsPage.ThoughtsList.title')}`, href: '/my-notes' }]}
       />
-      <PageTitle title="Збережене" subtitle="Твої улюблені поради та афірмації" />
+      <PageTitle title={t('SavedPage.title')} subtitle={t('SavedPage.subtitle')} />
       <SavedList />
     </div>
   )
