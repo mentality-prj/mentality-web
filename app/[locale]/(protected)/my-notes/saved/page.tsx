c8c54e9adac38cc575ec7735f8ae631eee07297b
petelkavictor <p.v.victor322@gmail.com>
2025-08-11 12:56:06 +0300

    feat: savedList

--- app/[locale]/(protected)/my-notes/saved/page.tsx
+++ app/[locale]/(protected)/my-notes/saved/page.tsx
@@ -1,3 +1,19 @@
+import { useTranslations } from 'next-intl'
+
+import { SavedList } from '@/components/SavedList'
+import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
+import { PageTitle } from '@/components/ui/PageTitle'
+
 export default function SavedPage() {
-  return <div className="">Saved Page</div>
+  const t = useTranslations('AffirmationsPage')
+  return (
+    <div className="flex flex-col gap-8">
+      <Breadcrumbs
+        currentPage={'Збережене'}
+        breadcrumbList={[{ title: `${t('ThoughtsList.title')}`, href: '/my-notes' }]}
+      />
+      <PageTitle title="Збережене" subtitle="Твої улюблені поради та афірмації" />
+      <SavedList />
+    </div>
+  )
 }
