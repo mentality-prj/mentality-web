0a9b55adb48c43f9953727683895831efee9fdb9
petelkavictor <p.v.victor322@gmail.com>
2025-08-14 22:01:57 +0300

    Feat/affirmations and tips list (#118)
    * feat: add mocked tips data
    
    * style: add new variant DailyCard
    
    * chore: add intl
    
    * feat: add sort
    
    * perf: filter
    
    * chore: add intl

---
+++ components/AffirmationsAndTips/AffirmationHistory.tsx
@@ -0,0 +1,33 @@
+import { getTranslations } from 'next-intl/server'
+
+import { mockAffirmations, mockTips } from '@/REST/mockApi'
+
+import { SectionCard } from '../ui/SectionCard'
+
+import { Filter } from './Filter'
+import { FilteredHistory } from './FilteredHistory'
+
+export const AffirmationHistory = async () => {
+  const affirmationsData = await mockAffirmations()
+  const tipsData = await mockTips()
+  const items = [
+    ...affirmationsData.map((item) => ({ ...item, type: 'affirmation' })),
+    ...tipsData.map((item) => ({ ...item, type: 'tip' })),
+  ]
+
+  const t = await getTranslations('AffirmationsPage')
+  return (
+    <>
+      {items.length > 0 ? (
+        <SectionCard title={t('sectionCard.title')}>
+          <div className="grid gap-6 laptop:grid-cols-[1fr_2.5fr]">
+            <Filter />
+            <FilteredHistory items={items} />
+          </div>
+        </SectionCard>
+      ) : (
+        <SectionCard title={t('sectionCard.title')} subtitle={t('sectionCard.subtitle')} />
+      )}
+    </>
+  )
+}
