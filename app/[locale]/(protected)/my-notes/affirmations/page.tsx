507fd332ea29df89901c8465c84912b21cbcf8ce
petelkavictor <p.v.victor322@gmail.com>
2025-08-14 22:04:36 +0300

    Revert "Feat/affirmations tips content (#115)"
    This reverts commit 64979039aefdcef41eccadcdb939104628c05287.

--- app/[locale]/(protected)/my-notes/affirmations/page.tsx
+++ app/[locale]/(protected)/my-notes/affirmations/page.tsx
@@ -1,31 +1,3 @@
-import { useTranslations } from 'next-intl'
-
-import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
-import { DailyCard } from '@/components/ui/DailyCard'
-import { PageTitle } from '@/components/ui/PageTitle'
-import { SectionCard } from '@/components/ui/SectionCard'
-import { AstronomyIcon } from '@/ds/icons/blueIcons/astronomy'
-import { StarRingIcon } from '@/ds/icons/blueIcons/star-ring'
-
 export default function AffirmationsPage() {
-  const t = useTranslations('AffirmationsPage')
-  return (
-    <div className="flex flex-col gap-8">
-      <Breadcrumbs
-        currentPage={t('title')}
-        breadcrumbList={[{ title: `${t('ThoughtsList.title')}`, href: '/my-notes' }]}
-      />
-      <PageTitle title={t('title')} subtitle={t('subtitle')} />
-      <div className="grid grid-cols-1 gap-6 laptop:grid-cols-2">
-        <DailyCard
-          variant="secondary"
-          title={t('affirmation.title')}
-          textContent={t('affirmation.text')}
-          icon={<StarRingIcon />}
-        />
-        <DailyCard variant="secondary" title={t('tip.title')} textContent={t('tip.text')} icon={<AstronomyIcon />} />
-      </div>
-      <SectionCard title={t('sectionCard.title')} subtitle={t('sectionCard.subtitle')} />
-    </div>
-  )
+  return <div className=""> Affirmations Page</div>
 }
