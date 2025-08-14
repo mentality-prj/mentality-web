b526591d505c42055d44fddea347425529e05080
petelkavictor <p.v.victor322@gmail.com>
2025-08-14 22:01:42 +0300

    Toast/affirmation (#117)
    * style: toast saved affirmation
    
    * chore: add intl

---
+++ components/ui/DailyCard/SavedToggle.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import toast from 'react-hot-toast'
+
+import { CheckIcon } from '@/ds/icons/check'
+import { StarIcon } from '@/ds/icons/star'
+import { Toggle } from '@/ds/shadcn/toggle'
+
+export const SavedToggle = ({ toastText }: { toastText?: string }) => {
+  return (
+    <Toggle
+      onPressedChange={(pressed) => {
+        if (pressed) {
+          toast.custom(
+            <div className="flex items-center gap-2 rounded-sm bg-outline-success px-8 py-4 text-reversed [&_svg]:size-6">
+              {toastText} <CheckIcon />
+            </div>,
+            {
+              position: 'bottom-right',
+              duration: 3000,
+            }
+          )
+        }
+      }}
+      className="fill-transparent p-0 text-textcolor-primary data-[state='on']:fill-primary data-[state='on']:text-primary [&_svg]:size-6"
+    >
+      <StarIcon />
+    </Toggle>
+  )
+}
