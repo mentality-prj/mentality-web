1433b4a9efb2b3e8a5a7d3e7f3768afa25360c64
petelkavictor <p.v.victor322@gmail.com>
2025-08-14 19:19:27 +0300

    chore: add intl

--- components/ui/DailyCard/DefaultCard.tsx
+++ components/ui/DailyCard/DefaultCard.tsx
@@ -1,4 +1,4 @@
-import { useLocale } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
 
 import { Button } from '@/ds/shadcn/button'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/ds/shadcn/card'
@@ -8,14 +8,13 @@ import { DefaultDailyCardProps } from '@/types/dailyCard'
 import { SupportedLanguage } from '@/types/languages'
 
 export const DefaultCard: React.FC<DefaultDailyCardProps> = ({ type, translations, className }) => {
+  const t = useTranslations('components.DailyCard')
   const locale = useLocale() as SupportedLanguage
   return (
     <Card className={cn('grid gap-2 border-none bg-surface-white p-4 shadow-none', className)}>
       <CardHeader className="p-0">
         <CardTitle className="flex min-h-[3rem] items-center text-base font-medium text-textcolor-tertiary">
-          <p className="m-0 line-clamp-2">
-            {type === 'affirmation' ? 'Ваша щоденна афірмація' : type === 'tip' ? 'Порада дня' : ''}
-          </p>
+          <p className="m-0 line-clamp-2">{t('title', { type })}</p>
         </CardTitle>
       </CardHeader>
 
@@ -26,7 +25,7 @@ export const DefaultCard: React.FC<DefaultDailyCardProps> = ({ type, translation
       <CardFooter className="mt-2 p-0">
         <Link href="/my-notes/affirmations">
           <Button className="ml-auto max-h-4 p-0" variant="textButton">
-            {type === 'affirmation' ? 'Всі афірмації' : type === 'tip' ? 'Всі поради' : ''}
+            {t('buttonText', { type })}
           </Button>
         </Link>
       </CardFooter>
