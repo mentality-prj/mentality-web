695e63cb21ee605f730bdb8121f5ee9a49eadd4c
petelkavictor <p.v.victor322@gmail.com>
2025-08-18 11:11:00 +0300

    chore: add title page with intl

--- app/[locale]/(protected)/my-progress/page.tsx
+++ app/[locale]/(protected)/my-progress/page.tsx
@@ -1,3 +1,12 @@
+import { useTranslations } from 'next-intl'
+
+import { PageTitle } from '@/components/ui/PageTitle'
+
 export default function MyProgress() {
-  return <div>My Progress Page</div>
+  const t = useTranslations('MyProgress')
+  return (
+    <div className="flex flex-col gap-8">
+      <PageTitle title={t('PageTitle.title')} subtitle={t('PageTitle.subtitle')} />
+    </div>
+  )
 }
