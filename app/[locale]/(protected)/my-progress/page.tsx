f01bdc5ea516441da36da4fca26952ae14de8a3d
petelkavictor <p.v.victor322@gmail.com>
2025-09-01 18:33:53 +0300

    style: add style activity calendar

--- app/[locale]/(protected)/my-progress/page.tsx
+++ app/[locale]/(protected)/my-progress/page.tsx
@@ -10,10 +10,12 @@ export default function MyProgress() {
   return (
     <div className="flex flex-col gap-8">
       <PageTitle title={t('PageTitle.title')} subtitle={t('PageTitle.subtitle')} />
+
       <div className="grid grid-cols-2 gap-4">
         <Activity />
         <TodayObservations />
       </div>
+
       <Achievements />
     </div>
   )
