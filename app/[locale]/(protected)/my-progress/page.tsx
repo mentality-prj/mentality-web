08af167e8e14eca49f809ee9cfffc0db9ad57ae6
petelkavictor <p.v.victor322@gmail.com>
2025-09-10 12:45:01 +0300

    Feat/dynamics chart (#134)
    * style: add style activity calendar
    
    * style: activity component
    
    * chore: add intl
    
    * chore: install chart
    
    * style: base styles for chart dynamics
    
    * feat: add switch for chart
    
    * style: update styles
    
    * chore: add intl

--- app/[locale]/(protected)/my-progress/page.tsx
+++ app/[locale]/(protected)/my-progress/page.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl'
 
 import { Achievements } from '@/components/Achievements'
 import { Activity } from '@/components/MyProgress/Activity'
+import { ChartDynamics } from '@/components/MyProgress/ChartDynamics'
 import { TodayObservations } from '@/components/MyProgress/TodayObservations'
 import { PageTitle } from '@/components/ui/PageTitle'
 
@@ -10,10 +11,13 @@ export default function MyProgress() {
   return (
     <div className="flex flex-col gap-8">
       <PageTitle title={t('PageTitle.title')} subtitle={t('PageTitle.subtitle')} />
+
       <div className="grid grid-cols-2 gap-4">
         <Activity />
         <TodayObservations />
       </div>
+      <ChartDynamics />
+
       <Achievements />
     </div>
   )
