b62c9e2e4ed1aa7c8c8f979d366734890b34316a
Oksana Gez <39303694+OksanaSanag@users.noreply.github.com>
2025-10-08 15:43:58 +0200

    feat: add Scale component for StressLevelScale (#143)
    * feat: add Scale component for StressLevelScale
    
    * style: fix imports (eslint)

---
+++ components/MoodTracker/StressLevelScale.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Scale } from '../../ds/components/Scale'
+
+export function StressLevelScale() {
+  const labelsArray = ['Дуже спокійно', 'Спокійно', 'Нейтрально', 'Трохи напружено', 'Дуже напружено']
+
+  const [value, setValue] = useState<number | null>(null)
+
+  return <Scale labels={labelsArray} value={value} onChange={setValue} />
+}
