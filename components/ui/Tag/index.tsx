82e71432b93940090cbb9c159583cb03ff8e3b76
petelkavictor <p.v.victor322@gmail.com>
2025-10-24 20:22:31 +0300

    style: fix error lint

--- components/ui/Tag/index.tsx
+++ components/ui/Tag/index.tsx
@@ -8,7 +8,7 @@ interface TagProps {
 export const Tag = ({ text, value }: TagProps) => {
   return (
     <ToggleGroupItem
-      className="bg-tag-surfaceLightPurple rounded-xs px-3 py-1 text-textcolor-secondary hover:bg-primary-hover hover:text-reversed focus:outline-none focus-visible:bg-primary-focus focus-visible:ring-1 focus-visible:ring-primary-focus focus-visible:ring-offset-4 active:bg-primary-pressed data-[state='on']:bg-primary data-[state='on']:text-reversed data-[state='on']:focus-visible:bg-primary-focus"
+      className="rounded-xs bg-tag-surfaceLightPurple px-3 py-1 text-textcolor-secondary hover:bg-primary-hover hover:text-reversed focus:outline-none focus-visible:bg-primary-focus focus-visible:ring-1 focus-visible:ring-primary-focus focus-visible:ring-offset-4 active:bg-primary-pressed data-[state='on']:bg-primary data-[state='on']:text-reversed data-[state='on']:focus-visible:bg-primary-focus"
       value={value}
     >
       {text}
