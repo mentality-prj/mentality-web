6bd21a7d0558fa06d7db2ce8a277c2e3b9a92a37
Taras Gez <taraspixel@gmail.com>
2025-11-10 22:08:57 +0100

    fix: add tranlations

--- components/Admin/AddTag.tsx
+++ components/Admin/AddTag.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useTranslations } from 'next-intl'
 import { useSession } from 'next-auth/react'
 
 import { tagproperties } from '@/constants/tags'
@@ -12,6 +13,7 @@ import { SUPPORTED_LANGUAGES, SupportedLanguage } from '@/types/languages'
 import { TagProperties } from '@/types/tags'
 
 export default function AddTag() {
+  const t = useTranslations('components.Admin.AddTag')
   const { data } = useSession()
   const session = data as CustomSession
 
@@ -33,19 +35,19 @@ export default function AddTag() {
 
   const tagPropertiesMap = tagproperties.map((prop: TagProperties) => (
     <div key={prop.key}>
-      <Label htmlFor={prop.key}>{prop.label}</Label>
+      <Label htmlFor={prop.key}>{t(`fields.${prop.name}.label`)}</Label>
       <Input required id={prop.key} name={prop.name} type="text" className="w-full max-w-xl" />
-      <span>{prop.description}</span>
+      <span>{t(`fields.${prop.name}.description`)}</span>
     </div>
   ))
 
   return (
     <form action={createTag} className="flex w-full flex-col items-center gap-4 py-6">
-      <h2>Add a new tag</h2>
+      <h2>{t('title')}</h2>
       {tagPropertiesMap}
       <div>
         <Button type="submit" className="flex-none" color="success">
-          Add Tag
+          {t('button')}
         </Button>
       </div>
     </form>
