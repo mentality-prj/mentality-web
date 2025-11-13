fd5694f074c1c3d04060dde817b2d999767b1e6d
petelkavictor <p.v.victor322@gmail.com>
2025-11-13 16:11:27 +0200

    fix: sidebar in mobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,18 +3,27 @@
 import { useSession } from 'next-auth/react'
 
 import LangSwitch from '@/components/Header/LangSwitch'
+import { HamburgerMenu } from '@/ds/icons/hamburger-menu'
+import { Button } from '@/ds/shadcn/button'
+import { useSidebar } from '@/ds/shadcn/sidebar'
 
 import { LocalDate } from './LocalDate'
 import { SearchBar } from './SearchBar'
 import { UserMenu } from './UserMenu'
 
 export function Header() {
+  const { isMobile, toggleSidebar } = useSidebar()
   const { data } = useSession()
   const user = data?.user
 
   if (!user) return null
   return (
     <header className="flex w-full items-center justify-between rounded-b-default bg-surface-white px-2 py-2 tablet:mb-4 tablet:px-4 desktop:mb-8 desktop:px-8 desktop:py-3">
+      {isMobile && (
+        <Button className="h-6 w-6" onClick={toggleSidebar} variant="iconButton" size="icon">
+          <HamburgerMenu />
+        </Button>
+      )}
       <LocalDate />
       <div className="flex items-center gap-1 desktop:gap-4">
         <SearchBar />
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,9 +2,6 @@
 import { ReactNode } from 'react'
 
 import Content from '@/components/wrappers/Content'
-import { HamburgerMenu } from '@/ds/icons/hamburger-menu'
-import { Button } from '@/ds/shadcn/button'
-import { useSidebar } from '@/ds/shadcn/sidebar'
 
 import SidebarWrapper from '../Sidebar'
 
@@ -13,15 +10,9 @@ interface MainLayoutProps {
 }
 
 const Layout = ({ children }: MainLayoutProps) => {
-  const { isMobile, toggleSidebar } = useSidebar()
   return (
     <SidebarWrapper>
       <section className="flex w-full">
-        {isMobile && (
-          <Button className="absolute left-3 top-3 h-6 w-6" onClick={toggleSidebar} variant="iconButton" size="icon">
-            <HamburgerMenu />
-          </Button>
-        )}
         <Content>{children}</Content>
       </section>
     </SidebarWrapper>
