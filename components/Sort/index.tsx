f934def238501a987f4eff44f731d0b4bcb23073
petelkavictor <p.v.victor322@gmail.com>
2025-11-13 17:12:10 +0200

    feat: add Sort component

---
+++ components/Sort/index.tsx
@@ -0,0 +1,28 @@
+import { DropdownInput } from '@/ds/components/DropdownInput'
+import { SortVerticalIcon } from '@/ds/icons/sort-vertical'
+import { useTranslations } from 'next-intl'
+import { use } from 'react'
+
+interface SortProps {
+  id: string
+  defaultValue: 'newest' | 'oldest'
+  onValueChange: (value: 'newest' | 'oldest') => void
+}
+
+export const Sort = ({ id, defaultValue, onValueChange }: SortProps) => {
+  const t = useTranslations('components.Sort')
+  const itemsSort = [
+    { value: 'newest', text: t('Newest') },
+    { value: 'oldest', text: t('Oldest') },
+  ]
+  return (
+    <DropdownInput
+      id={id}
+      label={t('Label')}
+      defaultValue={defaultValue}
+      onValueChange={(value) => onValueChange(value as 'newest' | 'oldest')}
+      labelIcon={<SortVerticalIcon />}
+      items={itemsSort}
+    />
+  )
+}
