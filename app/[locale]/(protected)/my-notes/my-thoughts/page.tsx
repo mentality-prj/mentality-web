0f338f6df522ea11bd3895e87cc73a0b487a4fc2
petelkavictor <p.v.victor322@gmail.com>
2025-11-13 15:41:16 +0200

    refactor: calendar on my-thouhgts page

--- app/[locale]/(protected)/my-notes/my-thoughts/page.tsx
+++ app/[locale]/(protected)/my-notes/my-thoughts/page.tsx
@@ -1,19 +1,31 @@
 import { useTranslations } from 'next-intl'
 
-import { CalendarThoughts } from '@/components/MyThoughts/CalendarThoughtsNotes/CalendarThoughtsNotes'
+import { Calendar } from '@/components/Calendar'
 import { ThoughtsFormWrapper } from '@/components/MyThoughts/Diary/ThoughtsFormWrapper'
 import { Breadcrumbs } from '@/components/ui/Breadcrumbs'
 import { PageTitle } from '@/components/ui/PageTitle'
 
 export default function MyThoughtsPage() {
   const t = useTranslations('MyThougtsPage')
+  const count = 0
   return (
     <div className="flex flex-col gap-8">
       <Breadcrumbs currentPage={t('title')} breadcrumbList={[{ title: t('ThoughtsList.title'), href: '/my-notes' }]} />
       <PageTitle title={t('title')} subtitle={t('subtitle')} />
       <div className="grid grid-cols-1 gap-6 laptop:grid-cols-2">
         <ThoughtsFormWrapper />
-        <CalendarThoughts />
+        <Calendar
+          selectedDays={[new Date()]}
+          title={t('CalendarThoughtsNotes.title')}
+          subtitle={
+            <p className="text-sm text-textcolor-secondary">
+              <span className="mr-1 text-xl font-semibold text-textcolor-primary">{count}</span>
+              {t('CalendarThoughtsNotes.subtitle')}
+            </p>
+          }
+          activeLabel="дні з записами"
+          inactiveLabel="дні без записів"
+        />
       </div>
     </div>
   )
