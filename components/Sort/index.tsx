e0e24af91dcd47942bcc99e2128335b7d98e7521
petelkavictor <p.v.victor322@gmail.com>
2025-11-14 15:36:51 +0200

    Feat/sort (#165)
    * style: add icon
    
    * feat: add Sort component
    
    * chore: update intl
    
    * refactor: add Sort on page
    
    * fix: imports
    
    * fix: replacing defaultValue with value

---
+++ components/Sort/index.tsx
@@ -0,0 +1,28 @@
+import { useTranslations } from 'next-intl'
+
+import { DropdownInput } from '@/ds/components/DropdownInput'
+import { SortVerticalIcon } from '@/ds/icons/sort-vertical'
+
+interface SortProps {
+  id: string
+  value: 'newest' | 'oldest'
+  onValueChange: (value: 'newest' | 'oldest') => void
+}
+
+export const Sort = ({ id, value, onValueChange }: SortProps) => {
+  const t = useTranslations('components.Sort')
+  const itemsSort = [
+    { value: 'newest', text: t('Newest') },
+    { value: 'oldest', text: t('Oldest') },
+  ]
+  return (
+    <DropdownInput
+      id={id}
+      label={t('Label')}
+      defaultValue={value}
+      onValueChange={(value) => onValueChange(value as 'newest' | 'oldest')}
+      labelIcon={<SortVerticalIcon />}
+      items={itemsSort}
+    />
+  )
+}
