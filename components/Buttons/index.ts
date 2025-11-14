055670572965cd06544ee0d9a3835ee7b089d702
petelkavictor <p.v.victor322@gmail.com>
2025-11-14 15:08:09 +0200

    chore: delete components

--- components/Buttons/index.ts
+++ components/Buttons/index.ts
@@ -1,5 +1,3 @@
-import GoToLoginPageButton from './GoToLoginPageButton'
-import SignOut from './SignOut'
 import SignOutButton from './SignOutButton'
 
-export { GoToLoginPageButton, SignOut, SignOutButton }
+export { SignOutButton }
--- components/Shop/ShopEmpty.tsx
+++
@@ -1,6 +0,0 @@
-import { useTranslations } from 'next-intl'
-
-export default function ShopEmpty() {
-  const t = useTranslations()
-  return <div>{t('ShopPage.ShopEmpty')}</div>
-}
