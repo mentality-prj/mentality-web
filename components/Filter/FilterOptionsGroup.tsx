f99e12ed10923221bea3a5f21c990c1884d5ed50
petelkavictor <p.v.victor322@gmail.com>
2025-11-30 17:07:08 +0200

    refactor: replace toogle-group-item with tag

--- components/Filter/FilterOptionsGroup.tsx
+++ components/Filter/FilterOptionsGroup.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from 'next-intl'
 
-import { ToggleGroup, ToggleGroupItem } from '@/ds/shadcn/toggle-group'
-import { cn } from '@/lib/utils'
+import { Tag } from '@/ds/components/Tag'
+import { ToggleGroup } from '@/ds/shadcn/toggle-group'
 
 export function FilterOptionGroup({
   value,
@@ -16,16 +16,7 @@ export function FilterOptionGroup({
   return (
     <ToggleGroup type="single" value={value} onValueChange={onChange} className="flex-wrap justify-start gap-2">
       {options.map((opt) => (
-        <ToggleGroupItem
-          key={opt}
-          value={opt}
-          aria-label={`Toggle ${opt}`}
-          className={cn(
-            "max-h-[22px] rounded-xs bg-secondary px-3 py-1 text-xs/[14px] text-textcolor-secondary data-[state='on']:bg-primary data-[state='on']:text-reversed"
-          )}
-        >
-          {t(`${opt}Tag`)}
-        </ToggleGroupItem>
+        <Tag text={t(`${opt}Tag`)} key={opt} value={opt} aria-label={`Toggle ${opt}`} />
       ))}
     </ToggleGroup>
   )
