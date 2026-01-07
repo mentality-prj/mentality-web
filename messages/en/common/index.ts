cd2eb81d053be63143ae19e79d32790eb6c9d9a1
petelkavictor <p.v.victor322@gmail.com>
2026-01-07 13:33:33 +0200

    refactor: remove PageTitle from i18n

--- messages/en/common/index.ts
+++ messages/en/common/index.ts
@@ -3,7 +3,6 @@ import Buttons from './Buttons.json'
 import General from './General.json'
 import menu from './menu.json'
 import My from './My.json'
-import PageTitle from './PageTitle.json'
 import SectionCard from './SectionCard.json'
 import title from './title.json'
 
@@ -13,7 +12,6 @@ export const common = {
   General,
   menu,
   My,
-  PageTitle,
   SectionCard,
   title,
 }
