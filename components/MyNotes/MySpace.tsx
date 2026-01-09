08fd8a3c6981e48a4f52de350cbb6819db430253
petelkavictor <p.v.victor322@gmail.com>
2026-01-09 21:10:45 +0200

    refactor: replace CustomCard with Card

--- components/MyNotes/MySpace.tsx
+++ components/MyNotes/MySpace.tsx
@@ -1,33 +1,33 @@
 import { getTranslations } from 'next-intl/server'
 
-import CustomCard from '@/ds/components/CustomCard'
 import { SectionCard } from '@/ds/components/SectionCard'
-import { ChatRoundLikeIcon } from '@/ds/icons/chat-round-like'
-import { NotesMinimalisticIcon } from '@/ds/icons/notes-minimalistic'
-import { StarRingIcon } from '@/ds/icons/star-ring'
-import { StarsIcon } from '@/ds/icons/stars'
+import { Link } from '@/i18n/navigation'
+
+import Card from '../Cards/Card'
 
 export async function MySpace() {
   const t = await getTranslations()
 
   const cardMyNotesData = [
-    { key: 'card1', link: '/my-thoughts', icon: <ChatRoundLikeIcon /> },
-    { key: 'card2', link: '/tests', icon: <NotesMinimalisticIcon /> },
-    { key: 'card3', link: '/affirmations', icon: <StarRingIcon /> },
-    { key: 'card4', link: '/saved', icon: <StarsIcon /> },
+    { key: 'card1', link: '/my-thoughts' },
+    { key: 'card2', link: '/tests' },
+    { key: 'card3', link: '/affirmations' },
+    { key: 'card4', link: '/saved' },
   ] as const
 
   return (
     <SectionCard title={t('common.SectionCard.title', { title: 'mySpace' })}>
       <div className="grid grid-cols-1 gap-8 laptop:grid-cols-2">
-        {cardMyNotesData.map(({ key, link, icon }) => (
-          <CustomCard
+        {cardMyNotesData.map(({ key, link }) => (
+          <Card
             key={key}
             title={t(`components.MySpaceCards.${key}.title`)}
             text={t(`components.MySpaceCards.${key}.desc`)}
-            hrefLink={`my-notes/${link}`}
-            textLink={t('common.Buttons.goTo')}
-            backgroundIcon={icon}
+            aftertext={
+              <Link className="text-blue-600 underline" href={`/my-notes/${link}`}>
+                {t('common.Buttons.goTo')}
+              </Link>
+            }
           />
         ))}
       </div>
