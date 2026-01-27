5772be4b4a188c6e05a2fc990ee72d3375c414af
petelkavictor <p.v.victor322@gmail.com>
2026-01-27 19:04:18 +0200

    chore: remove unused types

--- types/cart.ts
+++
@@ -1,9 +0,0 @@
-import { ShopItemCookiesProps, ShopItemProps } from '@/types/shop'
-
-export type CartItemProps = ShopItemProps & {
-  quantity: number
-}
-
-export type CartItemCookiesProps = ShopItemCookiesProps & {
-  quantity: number
-}
