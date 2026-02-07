50c0233348ca9093f549dbbfcaf653bdeb50004a
petelkavictor <p.v.victor322@gmail.com>
2026-02-07 22:01:36 +0200

    chore: remove unnecessary files

--- components/MyThoughts/NotesOfUser/UserNotesCardFilter.tsx
+++
@@ -1,7 +0,0 @@
-import CustomCard from '@/ds/components/CustomCard'
-
-export function UserNotesCardFilter() {
-  //  const t = useTranslations should be added
-
-  return <CustomCard text="Here will be filter" className="w-1/4" />
-}
--- components/MyThoughts/NotesOfUser/UserNotesWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import { getUserNotes } from '@/actions/notes.action'
-
-import { UserNotes } from './UserNotes'
-
-export async function UserNotesWrapper() {
-  const notes = await getUserNotes()
-  return <UserNotes notes={notes} />
-}
