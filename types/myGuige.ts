d6074f1da4994f961ad288014c0832b6bafb8921
petelkavictor <p.v.victor322@gmail.com>
2026-02-11 01:24:29 +0200

    refactor: remove old functionality

--- types/myGuige.ts
+++
@@ -1,9 +0,0 @@
-type MeditationDataCategory = 'meditations' | 'breathing' | 'calming'
-
-export type MeditationData = {
-  id: string
-  category: MeditationDataCategory
-  title: string
-  annotation: string
-  description: string
-}
