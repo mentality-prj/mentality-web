d73b94377346147ca13532a74c179ae5fa0fd720
petelkavictor <p.v.victor322@gmail.com>
2026-02-23 22:01:56 +0200

    chore: add intl

--- messages/pl/components/index.ts
+++ messages/pl/components/index.ts
@@ -15,6 +15,7 @@ import Header from './Header.json'
 import LastTenDaysMoodRecords from './LastTenDaysMoodRecords.json'
 import Mood from './Mood.json'
 import MoodBadge from './MoodBadge.json'
+import MoodCard from './MoodCard.json'
 import MoodMarks from './MoodMarks.json'
 import Navbar from './Navbar.json'
 import Navigation from './Navigation.json'
@@ -27,6 +28,7 @@ import StressLevelScale from './StressLevelScale.json'
 import Tags from './Tags.json'
 import TenDaysSummary from './TenDaysSummary.json'
 import TextareaWithLabel from './TextareaWithLabel.json'
+import TodayMoodNotes from './TodayMoodNotes.json'
 import TodayObservations from './TodayObservations.json'
 
 export const components = {
@@ -54,10 +56,12 @@ export const components = {
   Tags,
   TenDaysSummary,
   TextareaWithLabel,
+  TodayMoodNotes,
   TodayObservations,
   CookieConsent,
   Diary,
   AvatarMenu,
+  MoodCard,
   FavoriteButton,
   ContactForm,
 }
