60934f737eddb9e0cf76165f59cf0cc0222610dc
petelkavictor <p.v.victor322@gmail.com>
2026-03-13 21:38:27 +0200

    fix: add i18n signin(signout) button (#327)
    Co-authored-by: Taras Gez <37795572+TarasGez@users.noreply.github.com>

--- components/shared/Buttons/SignInButton.tsx
+++ components/shared/Buttons/SignInButton.tsx
@@ -1,9 +1,8 @@
-import { getLocale } from 'next-intl/server'
+import { getLocale, getTranslations } from 'next-intl/server'
 
 import { signIn } from '@/auth'
 import { ProviderKey, Providers } from '@/constants/providers'
 import { Routes } from '@/constants/routes'
-import { Texts } from '@/constants/texts'
 import { Button } from '@/ui/button'
 
 interface SignInButtonProps {
@@ -12,7 +11,8 @@ interface SignInButtonProps {
 
 export default async function SignInButton({ provider }: SignInButtonProps) {
   const providerName = Providers[`${provider}`]
-  const textBtn = String(`${Texts.LOGIN_WITH} ${providerName}`)
+  const t = await getTranslations('common.Buttons')
+  const textBtn = String(`${t('signInWith')} ${providerName}`)
   const locale = await getLocale()
 
   async function handleLogin() {
