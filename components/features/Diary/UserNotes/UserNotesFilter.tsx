afee345bf5540e27540bd2354566cb0647bd6400
petelkavictor <p.v.victor322@gmail.com>
2026-03-25 01:04:20 +0200

    fix: lint

--- components/features/Diary/UserNotes/UserNotesFilter.tsx
+++ components/features/Diary/UserNotes/UserNotesFilter.tsx
@@ -5,11 +5,11 @@ import { useTranslations } from 'next-intl'
 import { Filter } from '@/components/shared/Filter/Filter'
 import { FilterSection } from '@/components/shared/Filter/FilterSection'
 import { UserTagsFilterGroup } from '@/components/shared/Filter/UserTagsFilterGroup'
+import { WeekdaysFilterGroup } from '@/components/shared/Filter/WeekdaysFilterGroup'
 import { Sort } from '@/components/shared/Sort/Sort'
 import { useUserNotesFilters } from '@/context/userNotesFilterContext'
-import { UserTag } from '@/types/tags'
-import { WeekdaysFilterGroup } from '@/components/shared/Filter/WeekdaysFilterGroup'
 import { weekOptions } from '@/mappers/weekdays.mapper'
+import { UserTag } from '@/types/tags'
 
 type Props = {
   availableTags: UserTag[]
