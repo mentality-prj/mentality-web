7a3ecaad84528c9847a4b931f0ec36c4dc17e53d
petelkavictor <p.v.victor322@gmail.com>
2026-04-20 10:03:53 +0300

    fix: resolve conversation

--- ui/input.tsx
+++ ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'file:text-foreground placeholder:text-muted-foreground flex w-full rounded-full border bg-transparent px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium hover:border-primary-hover focus:border-primary-pressed focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          'file:text-foreground placeholder:text-muted-foreground flex w-full rounded-full border border-border bg-transparent px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium hover:border-primary-hover focus:border-primary-pressed focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
--- ui/textarea.tsx
+++ ui/textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
     return (
       <textarea
         className={cn(
-          'flex w-full resize-none rounded-md border bg-transparent px-4 py-3 text-sm font-normal text-tertiary hover:border-primary-hover focus:border-primary-pressed focus-visible:outline-none disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-background-muted',
+          'flex w-full resize-none rounded-md border bg-transparent px-4 py-3 text-sm font-normal text-tertiary hover:border-primary-hover focus:border-primary-pressed focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:border-border disabled:bg-background-muted',
           className
         )}
         ref={ref}
