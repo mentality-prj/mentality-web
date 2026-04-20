1c126d49ec108fb50394dba4e21f8fbbd20a2613
petelkavictor <p.v.victor322@gmail.com>
2026-04-20 10:07:48 +0300

    fix: update style for input and textarea (#388)
    * fix: update style for input and textarea
    
    * fix: resolve conversation

--- ui/input.tsx
+++ ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'border-input file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex w-full rounded-full border bg-transparent px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+          'file:text-foreground placeholder:text-muted-foreground flex w-full rounded-full border border-border bg-transparent px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium hover:border-primary-hover focus:border-primary-pressed focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
--- ui/textarea.tsx
+++ ui/textarea.tsx
@@ -7,8 +7,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
     return (
       <textarea
         className={cn(
-          //'placeholder:text-muted-foreground md:text-sm',
-          'disabled:border-disabled disabled:bg-disabled border-outline-secondary text-textcolor-tertiary invalid:border-outline-error focus:border-primary-focus focus:text-primary-focus focus-visible:ring-primary-focus flex w-full resize-none rounded-md border bg-transparent px-4 py-3 text-[14px]/[20px] font-normal invalid:text-textcolor-primary hover:border-primary-hover focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:bg-background-muted',
+          'flex w-full resize-none rounded-md border bg-transparent px-4 py-3 text-sm font-normal text-tertiary hover:border-primary-hover focus:border-primary-pressed focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:border-border disabled:bg-background-muted',
           className
         )}
         ref={ref}
