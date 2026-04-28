ff658c9ba2b2d3962707e64d2a74ed196b8a850b
petelkavictor <p.v.victor322@gmail.com>
2026-04-28 10:59:20 +0300

    style: remove border-radius

--- app/[locale]/company/page.tsx
+++ app/[locale]/company/page.tsx
@@ -37,7 +37,8 @@ export default async function CompanyDashboardPage({ params }: { params: Promise
         <div className="h-full w-1/2 bg-background-alt" />
       </div>
       <div className="container-max-width relative z-10 flex w-full">
-        <main className="old-paper min-h-screen w-full flex-1 flex-col items-start justify-between">
+        {/* TODO: temporary rounded is none because don't have sidebar in this page */}
+        <main className="old-paper min-h-screen w-full flex-1 flex-col items-start justify-between rounded-none">
           <div className="padded flex w-full flex-col gap-sm tablet:gap-6 sm:gap-md">
             <CompanyHeader />
             <PageTitle title={t('title')} />
