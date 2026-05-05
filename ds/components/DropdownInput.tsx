9dfe0f7fc237d25ea5639d62fa71fb276474a94f
petelkavictor <p.v.victor322@gmail.com>
2026-05-05 23:41:20 +0300

    fix: prettier order styles

--- ds/components/DropdownInput.tsx
+++ ds/components/DropdownInput.tsx
@@ -29,7 +29,7 @@ export const DropdownInput = ({
       <Select disabled={disabled} defaultValue={defaultValue} value={value} onValueChange={onValueChange}>
         <SelectTrigger
           id={id}
-          className="border-outline-secondary focus-visible:border-primary-focus disabled:border-disable disabled:text-disable data-[placeholder]:text-textcolor-tertiary [&_svg]:text-iconcolor-secondary [&_svg]:focus-visible:text-primary-focus [&_svg]:disabled:text-disable peer h-12 w-full rounded-md px-[14px] py-4 shadow-none outline-none ring-0 hover:border-primary-hover focus:ring-0 focus-visible:border-[3px] data-[state='open']:border-primary [&_svg]:opacity-100 [&_svg]:hover:text-primary-hover [&_svg]:data-[state=open]:rotate-180 [&_svg]:data-[state=open]:text-primary"
+          className="border-outline-secondary disabled:border-disable disabled:text-disable data-[placeholder]:text-textcolor-tertiary [&_svg]:text-iconcolor-secondary [&_svg]:disabled:text-disable peer h-12 w-full rounded-md px-[14px] py-4 shadow-none outline-none ring-0 hover:border-primary-hover focus:ring-0 focus-visible:border-[3px] focus-visible:border-primary-focus data-[state='open']:border-primary [&_svg]:opacity-100 [&_svg]:hover:text-primary-hover [&_svg]:focus-visible:text-primary-focus [&_svg]:data-[state=open]:rotate-180 [&_svg]:data-[state=open]:text-primary"
         >
           <SelectValue placeholder={placeholder} />
         </SelectTrigger>
@@ -49,7 +49,7 @@ export const DropdownInput = ({
       </Select>
       <label
         className={cn(
-          "peer-focus-visible:text-primary-focus peer-disabled:text-disable mb-2 text-sm/4 peer-data-[state='open']:text-primary",
+          "peer-disabled:text-disable mb-2 text-sm/4 peer-focus-visible:text-primary-focus peer-data-[state='open']:text-primary",
           labelIcon && 'mb-5 flex items-center gap-0.5 text-base'
         )}
         htmlFor={id}
