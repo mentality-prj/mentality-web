e56e07531048d2dbb8cb5da3510370f03a01f91b
petelkavictor <p.v.victor322@gmail.com>
2026-05-06 18:06:35 +0300

    style: replace emoji for mood-note (#398)
    * style: replace emoji for mood-note
    
    * fix: correct aspect ratio of emoji
    
    * fix:  balancing concentration, energy, and stress assesments
    
    * style: change neutral emoji
    
    * fix: error property

--- components/features/MoodTracker/EnergyAssessment/EnergyAssessment.tsx
+++ components/features/MoodTracker/EnergyAssessment/EnergyAssessment.tsx
@@ -12,7 +12,7 @@ export function EnergyAssessment({ value, onChange }: EnergyAssessmentProps) {
   const tm = useTranslations('components.Mood')
 
   return (
-    <div className="flex w-40 flex-col">
+    <div className="flex w-40 flex-col justify-between">
       <h6 className="pl-4">{tm('chooseEnergy')}</h6>
       <div className="p-4">
         <EnergyLevelScale value={value} onChange={onChange} />
