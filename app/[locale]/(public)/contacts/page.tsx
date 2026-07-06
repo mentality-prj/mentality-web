60ee3ef01611e7dfffe63f830ba5672d0b061365
petelkavictor <p.v.victor322@gmail.com>
2026-07-06 10:20:34 +0300

    style: add horizontal padding

--- app/[locale]/(public)/contacts/page.tsx
+++ app/[locale]/(public)/contacts/page.tsx
@@ -1,7 +1,11 @@
 import { ContactForm } from '@/components/features/Contact/ContactForm'
 
 const ContactPage = () => {
-  return <ContactForm />
+  return (
+    <div className="px-4 tablet:px-6 md:px-8 lg:px-10">
+      <ContactForm />
+    </div>
+  )
 }
 
 export default ContactPage
