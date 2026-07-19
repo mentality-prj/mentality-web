526986f932203a1da3bd739c1e2b769df003b700
petelkavictor <p.v.victor322@gmail.com>
2026-07-19 22:07:46 +0300

    style: update mood-tracker page

--- app/[locale]/(protected)/mood-tracker/page.tsx
+++ app/[locale]/(protected)/mood-tracker/page.tsx
@@ -34,7 +34,7 @@ export default async function MoodTracker({
   return (
     <div className="flex flex-col gap-md">
       <PageTitle title={t('title')} subtitle={t('subtitle')} />
-      <div className="flex w-full flex-col gap-xs tablet:gap-md desktop:flex-row">
+      <div className="flex w-full flex-col gap-sm tablet:gap-md desktop:flex-row">
         <div className="desktop:w-3/5">
           <NewMoodNoteSection />
         </div>
